0f955df58550e592c74a945b67277adbe7d284bb
hannah <1065316432@qq.com>
2018-10-23 23:15:43 +0800

    feat: 一对一会话

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+let userList = [];
+const socketList = {};
 
-app.get('*', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/'));
+app.get('/client', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
 });
-//io.emit('some event', { for: 'everyone' });
 
-io.on('connection', function(socket){
-  // socket.broadcast.emit('hi');
-  socket.on('chat',function(msg){
-    console.log('msg: ',msg,new Date().valueOf());
-    io.emit('chat',msg);
+io.on('connection', function (socket) {
+  socket.on('register', function (name) {
+    userList.unshift({id:socket.id,name:name,status:'on'});
+    socketList[socket.id] = socket;
+    io.emit('userList', userList);
+  });
 
-  })
-  socket.on('disconnect', function(){
-    console.log('user disconnected ',new Date().valueOf());
+  socket.on('private', function (talk) {
+    const toSocket = socketList[talk.to.id];
+    toSocket.emit('private',talk);
+  });
+
+  socket.on('disconnect', function () {
+    const user = userList && userList.filter(item => item.id === socket.id);
+    const name = user && user[0] && user[0]['name'];
+    console.log('user disconnected ', name,socket.id);
+    userList.forEach(item=>{
+      if(item.id === socket.id){
+        item.status = 'off';
+      }
+    });
+    io.emit('userList', userList);
   });
 });
 
-http.listen(3000, function(){
+
+
+
+// var news = io
+//   .of('/news')
+//   .on('connection', function (socket) {
+//     socket.emit('new', 'new1');
+//     setTimeout(function(){
+//       socket.emit('new', 'new2');
+//     },30000);
+//     setTimeout(function(){
+//       socket.emit('new', 'new3');
+//     },600000);
+//   });
+
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
+
+function randomColor() {
+  return `rgb(${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)},${Math.ceil(Math.random()*255)})`
+}
