ba09430fd6dbd7fa5302b69ac17576a2fdac87fe
hannah <1065316432@qq.com>
2018-10-24 16:42:33 +0800

    feat: 服务端配置心跳频率及过期参数

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http,{pingInterval: 10000,pingTimeout:5000});
 var path = require('path');
 let userList = [];
 const socketList = {};
